6022cd9ee4e85f0ceaa465c1861bed1ff40401d2
deemac <deemac1623@gmail.com>
2019-12-01 22:41:12 +0000

    lesson 22 nomster

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -15,9 +15,13 @@ before_action :authenticate_user!, only: [:new, :create]
   end
 
   def show
-    @place = Place.find(params[:id]
+    @place = Place.find(params[:id])
   end
-  
+
+  def edit
+    @place = Place.find(params[:id])
+  end
+
 
   private
 
