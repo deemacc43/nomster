6783e616c22076444862ffdb74fd0c30f4349ae9
deemac <deemac1623@gmail.com>
2019-12-03 20:08:27 +0000

    lesson 24 nomster

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -16,15 +16,24 @@ before_action :authenticate_user!, only: [:new, :create]
 
   def show
     @place = Place.find(params[:id])
+    
+
   end
 
   def edit
     @place = Place.find(params[:id])
-    @place.update_attributes(place_params)
-    redirect_to root_path
-  end
+   end
 
   def update
+      @place = Place.find(params[:id])
+     @place.update_attributes(place_params)
+     redirect_to root_path
+  end
+
+  def destroy
+    @place = Place.find(params[:id])
+    @place.destroy
+    redirect_to root_path
   end
 
 
