541c9985d9024ab5d52452dff87305690483fc5e
deemac <deemac1623@gmail.com>
2019-12-12 20:40:09 +0000

    lesson 32 nomster

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -61,8 +61,11 @@ end
   def place_params
     params.require(:place).permit(:name, :description, :address);
   end #lines 59-61
-end
 
+  def show
+    @place = Place.find(params[:id])
+end
+ end
  
 
 
