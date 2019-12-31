a8bc80ef81e155a08e07da781c23d2dff9565d4b
deemac <deemac1623@gmail.com>
2019-12-31 16:51:23 +0000

    lesson 41 nomster

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
  config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
+    address: "smpt.gmail.com",
     port: 587,
     domain: "example.com",
     authentication: "plain",
